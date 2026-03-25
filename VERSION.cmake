fa77a83776d2efce6204e80fab6ffe3bde4bd17d
Karl Fleischmann <fleischmann.karl@mailbox.org>
2026-03-25 15:08:09 +0100

    v0.6.3

--- VERSION.cmake
+++ VERSION.cmake
@@ -1,9 +1,9 @@
-set(ckb-next_VERSION "0.6.2")
+set(ckb-next_VERSION "0.6.3")
 set(ckb-next_VERSION_MAJOR 0)
 set(ckb-next_VERSION_MINOR 6)
-set(ckb-next_VERSION_PATCH 2)
+set(ckb-next_VERSION_PATCH 3)
 # This should be set to TRUE _only_ in archive/tarball releases
-set(ckb-next_VERSION_IS_RELEASE FALSE)
+set(ckb-next_VERSION_IS_RELEASE TRUE)
 
 # Profile format version
 set(ckb-next_PROFILE_VER 3)
@@ -12,4 +12,4 @@ set(ckb-next_PROFILE_VER 3)
 set(ckb-next_SETTINGS_VER 1)
 
 # Copyright year string
-set(ckb-next_COPYRIGHT_YEAR "2025")
+set(ckb-next_COPYRIGHT_YEAR "2026")
